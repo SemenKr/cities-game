ac0ced2f3a9e7c7f6786870d54f954e01e39667c
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-11-23 21:39:33 +0300

    Add LooseScreen component
    A new component named LooseScreen has been created with a fundamental structure ready for further development. This component will display whenever the user loses the game, enhancing user interaction and gameplay experience.

---
+++ src/components/LooseScreen.tsx
@@ -0,0 +1,17 @@
+// LooseScreen.tsx
+import { FC } from 'react';
+
+interface LooseScreenProps {
+    // You can add more props if needed
+}
+
+const LooseScreen: FC<LooseScreenProps> = () => {
+    return (
+        <div>
+            {/* Add your loose screen content here */}
+            <h1>You Loose!</h1>
+        </div>
+    );
+};
+
+export default LooseScreen;
