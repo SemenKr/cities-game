48a392a4500bec9d16268809e82e54cd0fc7846b
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-11-23 21:38:52 +0300

    Add new WinScreen component
    Implemented a new component, WinScreen, to announce the player's victory. This component can be customised to show different content, as indicated by the placeholder HTML comment. WinScreen is designed to accept additional props, as needed.

---
+++ src/components/WinScreen.tsx
@@ -0,0 +1,17 @@
+// WinScreen.tsx
+import { FC } from 'react';
+
+interface WinScreenProps {
+    // You can add more props if needed
+}
+
+const WinScreen: FC<WinScreenProps> = () => {
+    return (
+        <div>
+            {/* Add your win screen content here */}
+            <h1>You Win!</h1>
+        </div>
+    );
+};
+
+export default WinScreen;
